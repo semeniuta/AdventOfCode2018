4aeaa465a85580f425f438f3084e9c6e10f9793d
Oleksandr Semeniuta <oleksandr.semeniuta@gmail.com>
2018-12-02 15:56:27 +0100

    Solution to day 2 part 2

--- 02_checksum/main.go
+++ 02_checksum/main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
+	"log"
 	"fmt"
-	"github.com/semeniuta/AdventOfCode2018/aoccommons"
 	"strings"
+	"github.com/semeniuta/AdventOfCode2018/aoccommons"
 )
 
 func readLines(filename string) []string {
@@ -81,14 +82,107 @@ func computeChecksum(lines []string) int {
 	return n2 * n3
 }
 
+func compareLines(line1 string, line2 string) []bool {
+
+	nChars := len(line1)
+	if nChars != len(line2) {
+		log.Fatalln("String of different length are supplied to compareLines", line1, line2)
+	}
+	
+	diff := make([]bool, nChars, nChars)
+
+	for i := 0; i < nChars; i++ {
+		if line1[i] == line2[i] {
+			diff[i] = true
+		} 
+	}
+
+	return diff
+
+}
+
+func moreThanOneDifferent(diff []bool) bool {
+	
+	var nDifferent int
+	var moreThanOne bool
+	
+	for _, match := range diff {
+		if !match {
+			nDifferent++
+		}
+		if nDifferent > 1 {
+			moreThanOne = true
+			break
+		}
+	}
+
+	return moreThanOne
+
+}
+
+func getCommonChars(line string, matches []bool) string {
+
+	characters := make([]byte, len(line), len(line))
+
+	for charIdx, match := range matches {
+		if match {
+			characters[charIdx] = line[charIdx]
+		}
+	}
+
+	return string(characters)
+}
+
+func findSimilar(lines []string) string {
+
+	type pair struct {
+		a int
+		b int
+	}
+
+	alreadyCompared := make(map[pair]bool)
+	
+	for i, line1 := range(lines) {
+
+		for j, line2 := range(lines) {
+
+			if i == j {
+				continue
+			}
+
+			if alreadyCompared[pair{i, j}] {
+				continue
+			}
+			
+			diff := compareLines(line1, line2)
+			oneCharDifference := !moreThanOneDifferent(diff)
+
+			if oneCharDifference {
+				return getCommonChars(line1, diff)
+			}
+
+			alreadyCompared[pair{i, j}] = true
+			alreadyCompared[pair{j ,i}] = true
+		}
+	}
+
+	return ""
+
+}
+
 func main() {
 
 	// Correct checksum: 7350
+	// Correct common characters: wmlnjevbfodamyiqpucrhsukg
 
 	filename := "input.txt"
 	lines := readLines(filename)
 
 	checksum := computeChecksum(lines)
 	fmt.Println("Checksum:", checksum)
+
+	similar := findSimilar(lines)
+	fmt.Println(similar)
+	
 
 }
